765ccdf9576096f59bda332146473c21db4a8f25
NoahHeidrich <noah.heidrich@boehringer-ingelheim.com>
2022-11-25 10:47:23 +0100

    Init

---
+++ src/main/java/com/example/lieferservice/LieferserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lieferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LieferserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LieferserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lieferservice/LieferserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lieferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LieferserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
