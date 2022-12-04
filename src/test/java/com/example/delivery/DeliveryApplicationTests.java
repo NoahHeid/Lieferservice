7fb674b519d6b513f38b58fef1b91cc3cf3a501b
Noah Heidrich <noah.heidrich@gmail.com>
2022-12-04 17:10:12 +0100

    Making build success

--- src/test/java/com/example/delivery/DeliveryApplicationTests.java
+++ src/test/java/com/example/delivery/DeliveryApplicationTests.java
@@ -2,8 +2,12 @@ package com.example.delivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ContextConfiguration
+@TestPropertySource("/test.properties")
 class DeliveryApplicationTests {
 
     @Test
